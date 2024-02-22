c3eeabbc4c590416d7e05f994a1b4799161acd84
blake-goodwyn <blackwoodthird@gmail.com>
2024-02-22 17:36:35 -0500

    update extract

--- scripts/extract_and_clean.py
+++ scripts/extract_and_clean.py
@@ -16,9 +16,9 @@ ingredient_counter = Counter()
 def preprocess_phrase(phrase):
     # Insert space before and after digits and special characters
     phrase = cor.correct_text(phrase)
-    #phrase = re.sub(r"([0-9]+)", "", phrase)
-    #phrase = re.sub(r"([^\w\s])", "", phrase)
-    #phrase = re.sub(r"(\d+\s?⁄\s?\d+)", "", phrase)
+    phrase = re.sub(r"([0-9]+)", r" \1 ", phrase)
+    phrase = re.sub(r"([^\w\s])", r" \1 ", phrase)
+    phrase = re.sub(r"(\d+\s?⁄\s?\d+)", r" \1 ", phrase)
     return phrase
 
 def extract_ingredient(phrase):
@@ -31,8 +31,14 @@ def extract_ingredient(phrase):
     ingredient_started = False
 
     for token in doc:
-        if token.pos_ == "NOUN" and (token.dep_ == "dobj" or token.dep_ == "compound" or token.dep_ == "nsubj"):
-            return token.text
+        if token.text.lower() in quantity_units or token.pos_ == 'NUM':
+            ingredient_started = True
+        elif ingredient_started and (token.pos_ in ['NOUN', 'ADJ']) and (token.text.lower() not in non_ingredient_keywords):
+            if token.dep_ == 'compound' or (token.head.pos_ == 'NOUN' and token.head.dep_ != 'appos'):
+                main_ingredient += token.text + ' '
+            elif token.pos_ == 'NOUN' and not main_ingredient:
+                main_ingredient += token.text + ' '
+                break  # Stop after finding the first main ingredient
 
     main_ingredient = main_ingredient.strip().lower()
     print("OUTPUT: ", main_ingredient)
