4902dd7d9fc6209110fca24e9c6b4d5569ad8d1f
blake-goodwyn <blackwoodthird@gmail.com>
2024-03-09 18:55:29 +0000

    cleaned up the good datasets

--- scripts/scratch1.py
+++ scripts/scratch1.py
@@ -3,12 +3,11 @@ import os
 from recipe_state_clusters import find_state_clusters
 from hmm_test import hmm_model
 
-cwd = os.getcwd()
-
+# Clean all the good datasets
 for file in os.listdir('data/GOOD DATASETS'):
     print("Linting: ", file)
     if file.endswith('.csv'):
-        lint(os.path.abspath(file))
+        lint(os.path.join(os.path.dirname(file),'data\\GOOD DATASETS', file))
 
 #find_state_clusters(file, sample=1000, max_clusters=20)
 #hmm_model()
