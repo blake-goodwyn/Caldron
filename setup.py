e80eb11f63d984c4ed8f3cc23817b57402aeef0a
blake-goodwyn <blackwoodthird@gmail.com>
2024-03-14 17:08:14 +0000

    reorg'd

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ebakery',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'tqdm',
+        'numpy',
+        'pandas',
+        'networkx',
+        'matplotlib',
+        'scikit-learn',
+        'spacy',
+        'nltk',
+        'gensim',
+        'requests',
+        'sqlalchemy',
+        'python-dotenv',
+    ]
+)
