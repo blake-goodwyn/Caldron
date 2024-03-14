6a4c8f5b5122b704868ff9cf4cb4ce0049344527
blake-goodwyn <blackwoodthird@gmail.com>
2024-03-14 16:42:03 +0000

    modified setup.py; added init

--- scripts/setup.py
+++ scripts/setup.py
@@ -18,4 +18,15 @@ setup(
         'sqlalchemy',
         'python-dotenv',
     ],
+    entry_points={
+        'console_scripts': [
+            'bipartite=ebakery.scripts.bipartite:main',
+            'class_defs=ebakery.scripts.class_defs:main',
+            'food_data=ebakery.scripts.food_data:main',
+            'genai_tools=ebakery.scripts.genai_tools:main',
+            'hmm_test=ebakery.scripts.hmm_test:main',
+            'recipe_collector=ebakery.scripts.recipe_collector:main',
+            'recipe_state_clusters=ebakery.scripts.recipe_state_clusters:main'
+        ]
+    }
 )
